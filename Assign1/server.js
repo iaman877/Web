29781b10adfda91c9fe21fd9a83316bcc7e7e584
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-02-25 18:58:03 +0530

    initial commit

---
+++ Assign1/server.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./model/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'v923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhhg'
+
+mongoose.connect('mongodb://localhost:27017/login-app-db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const app = express()
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json());
+
+app.post('/api/login', async (req, res) => {
+	const { username, password } = req.body
+	const user = await User.findOne({ username }).lean()
+
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+
+		const token = jwt.sign(
+			{
+				id: user._id,
+				username: user.username
+			},
+			JWT_SECRET
+		)
+
+		return res.json({ status: 'ok', data: token })
+	}
+
+	res.json({ status: 'error', error: 'Invalid username/password' })
+})
+
+
+app.post('/api/register', async (req, res) => {
+	const { username, password: plainTextPassword } = req.body
+
+	if (!username || typeof username !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+	const password = await bcrypt.hash(plainTextPassword, 10)
+
+	try {
+		const response = await User.create({
+			username,
+			password,
+            email,
+            date
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			return res.json({ status: 'error', error: ' Something wrong' })
+		}
+		throw error
+	}
+	res.json({ status: 'ok' })
+})
+
+
+app.listen(3000, () => {
+	console.log('Server up at 3000 port')
+})
